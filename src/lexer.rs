b2d12ec41e245d6748265e549e2e07656a469926
Tad Scritchfield <tadscritch@gmail.com>
2020-08-22 20:34:05 -0500

    Create initial lexer

---
+++ src/lexer.rs
@@ -0,0 +1,223 @@
+use std::iter::Peekable;
+use std::str::{Chars};
+
+
+#[derive(Clone, Debug)]
+pub enum TokenType {
+    Unknown,
+    Literal,
+
+    KeyType,
+
+    TInt,
+    TString,
+
+    OpPlus,
+    OpMinus,
+    OpStar,
+    OpForwSlash,
+    OpExclamation,
+    OpArrow,
+
+    ParenL,
+    ParenR,
+    BracketL,
+    BracketR,
+
+    Colon,
+
+    EOF,
+}
+
+#[derive(Clone, Debug)]
+pub struct Token {
+    t: TokenType,
+    value: String,
+    loc: (usize, usize)
+}
+
+impl Token {
+    fn eof(index: usize) -> Token {
+        Token {
+            t: TokenType::EOF,
+            value: "".to_string(),
+            loc: (index, index)
+        }
+    }
+}
+
+pub struct Lexer<'a> {
+    chars: Peekable<Chars<'a>>,
+    index: usize,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(src: &str) -> Lexer {
+        Lexer {
+            chars: src.chars().peekable(),
+            index: 0,
+        }
+    }
+
+    pub fn run(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
+
+        loop {
+            let next = self.next();
+
+            if let TokenType::EOF = next.t {
+                tokens.push(next);
+                break;
+            } else {
+                tokens.push(next);
+            }
+        }
+
+        tokens
+    }
+
+    fn next(&mut self) -> Token {
+        let mut c = if let Some(c) = self.peek() {
+            *c
+        } else {
+            return Token::eof(self.index);
+        };
+
+        println!("{} at index: {}", c, self.index);
+
+        while WHITESPACE.contains(&c) {
+            let next_char = self.advance();
+            println!("newC {:?}", next_char);
+            if next_char == None || self.peek() == None {
+                println!("get out of there!");
+                return Token::eof(self.index);
+            }
+
+            c = *self.peek().unwrap();
+        }
+
+        if is_special_identifier(Some(&c)) {
+            if let Some(si) = self.next_special_identifier() {
+                return si;
+            }
+        }
+
+        if is_valid_identifier(Some(&c)) {
+            return self.next_identifier();
+        }
+
+        let loc = (self.index, self.index + 1);
+        let token = match c {
+            '+' => Token { t: TokenType::OpPlus, value: String::new(), loc },
+            '-' => Token { t: TokenType::OpMinus, value: String::new(), loc },
+            '*' => Token { t: TokenType::OpStar, value: String::new(), loc },
+            '/' => Token { t: TokenType::OpForwSlash, value: String::new(), loc },
+            '!' => Token { t: TokenType::OpExclamation, value: String::new(), loc },
+            '(' => Token { t: TokenType::ParenL, value: String::new(), loc },
+            ')' => Token { t: TokenType::ParenR, value: String::new(), loc },
+            '{' => Token { t: TokenType::BracketL, value: String::new(), loc },
+            '}' => Token { t: TokenType::BracketR, value: String::new(), loc },
+            ':' => Token { t: TokenType::Colon, value: String::new(), loc },
+            _ => Token { t: TokenType::Unknown, value: String::new(), loc }
+        };
+
+        self.advance();
+
+        token
+    }
+
+    fn peek(&mut self) -> Option<&char> {
+        self.chars.peek()
+    }
+
+    fn advance(&mut self) -> Option<char> {
+        self.index += 1;
+        self.chars.next()
+    }
+
+    fn back(&mut self, index: usize) {
+        self.index = index;
+        self.chars.nth(index);
+    }
+
+    fn next_identifier(&mut self) -> Token {
+        let start = self.index;
+        let mut end = start;
+        let letter_regex = regex::Regex::new(r"[A-z'_]").unwrap();
+        let mut value = String::new();
+
+        while is_valid_identifier(self.peek()) {
+            let c = *self.peek().unwrap();
+
+            if end - start > 0 && !letter_regex.is_match(&c.to_string()) {
+                break;
+            }
+
+            end += 1;
+            value.push(c);
+            self.advance();
+        }
+
+        let t = match value.as_ref() {
+            "type" => TokenType::KeyType,
+            "Int" => TokenType::TInt,
+            "String" => TokenType::TString,
+            _ => TokenType::Literal,
+        };
+
+        Token {
+            t: t,
+            value: value,
+            loc: (start, end)
+        }
+    }
+
+    fn next_special_identifier(&mut self) -> Option<Token> {
+        let start = self.index;
+        let mut end = start;
+        let special_regex = regex::Regex::new(r"[->]").unwrap();
+        let mut value = String::new();
+
+        while is_special_identifier(self.peek()) {
+            let c = *self.peek().unwrap();
+
+            if end - start > 0 && !special_regex.is_match(&c.to_string()) {
+                break;
+            }
+
+            end += 1;
+            value.push(c);
+            self.advance();
+        }
+
+        match value.as_ref() {
+            "->" => Some(Token { t: TokenType::OpArrow, value, loc: (start, end) }),
+            _ => {
+                self.back(start);
+                None
+            }
+        }
+    }
+}
+
+fn is_valid_identifier(c: Option<&char>) -> bool {
+    if let Some(c) = c {
+        regex::Regex::new(r"[^\s\n\r0-9\+-/\*\^!\(\)\{\}=\.,:;|]")
+            .unwrap()
+            .is_match(&c.to_string())
+    } else {
+        false
+    }
+}
+
+fn is_special_identifier(c: Option<&char>) -> bool {
+    if let Some(c) = c {
+        regex::Regex::new(r"[->]")
+            .unwrap()
+            .is_match(&c.to_string())
+    } else {
+        false
+    }
+}
+
+const WHITESPACE: [char; 4] = [' ', '\n', '\r', '\t'];
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+mod lexer;
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let contents = fs::read_to_string("tests/test1.odet")?;
+
+    let mut l = lexer::Lexer::new(contents.as_str());
+    let tokens = l.run();
+
+    println!("{:?}", tokens);
+
+    Ok(())
 }
