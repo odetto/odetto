a5d21a26a0c0e84a546eb37cda5ed975f57a3910
Tad Scritchfield <tadscritch@gmail.com>
2020-09-22 20:02:58 -0500

    Add more error types

--- src/_helpers.rs
+++ src/_helpers.rs
@@ -13,6 +13,13 @@ pub enum ParseError {
     UnknownError,
 
     NoFieldsError,
+    DuplicateModelIdentifierError(TokenInfo),
+    MissingColonError(TokenInfo),
+    MissingFieldTypeError(TokenInfo),
+    GenericFieldTypeError(TokenInfo),
+    MissingRightBracketError(TokenInfo),
+    ExpectedFieldIdentifierError(TokenInfo),
+    DuplicateFieldIdentifierError(TokenInfo),
     GenericError(TokenInfo)
 }
 
@@ -21,7 +28,14 @@ impl fmt::Display for ParseError {
         match *self {
             ParseError::UnknownError => write!(f, "Uknown parsing error\n"),
             ParseError::NoFieldsError => write!(f, "No fields present on type. \n"),
-            ParseError::GenericError(ref info) => write!(f, "Generic parsing error at ({}, {})", info.loc.0, info.loc.1)
+            ParseError::DuplicateModelIdentifierError(ref info) => write!(f, "Duplicate model identifier: ({}, {})", info.loc.0, info.loc.1),
+            ParseError::MissingColonError(ref info) => write!(f, "Missing colon to indicate type on field: ({}, {})", info.loc.0, info.loc.1),
+            ParseError::MissingFieldTypeError(ref info) => write!(f, "Missing type on field: ({}, {})", info.loc.0, info.loc.1),
+            ParseError::GenericFieldTypeError(ref info) => write!(f, "Something is wrong with the type on field: ({}, {})", info.loc.0, info.loc.1),
+            ParseError::MissingRightBracketError(ref info) => write!(f, "Missing '}}' to close the type definition: ({}, {})", info.loc.0, info.loc.1),
+            ParseError::ExpectedFieldIdentifierError(ref info) => write!(f, "Expected a field identifier: ({}, {})", info.loc.0, info.loc.1),
+            ParseError::DuplicateFieldIdentifierError(ref info) => write!(f, "Duplicate field identifier: ({}, {})", info.loc.0, info.loc.1),
+            ParseError::GenericError(ref info) => write!(f, "Generic parsing error: ({}, {})", info.loc.0, info.loc.1)
         }
     }
 }
