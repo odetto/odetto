2fe1823ae075344b08c5c8a574247ee80a9e6b19
Tad Scritchfield <tadscritch@gmail.com>
2020-11-22 16:23:44 -0600

    add timing to tests

--- tests/test-files.rs
+++ tests/test-files.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::time::{Instant};
 
 use odetto::{lexer, parser, ast, _helpers::ParseError};
 
@@ -59,28 +60,15 @@ fn annotation_parser() {
 }
 
 fn test_file(file_name: &str) -> Result<ast::Root, ParseError> {
+    let start = Instant::now();
     let contents = fs::read_to_string(file_name).unwrap();
-    // let contents = fs::read_to_string("tests/3-required-parser.odet")?;
-    // let contents = fs::read_to_string("tests/4-array-parser.odet")?;
-    // let contents = fs::read_to_string("tests/5-better-parser.odet")?;
-    // let contents = fs::read_to_string("tests/6-duplicate-field-name.odet")?;
-    // let contents = fs::read_to_string("tests/7-missing-type-parser.odet")?;
-    // let contents = fs::read_to_string("tests/8-comments-parser.odet")?;
-    // let contents = fs::read_to_string("tests/9-annotation-parser.odet")?;
-
     let mut l = lexer::Lexer::new(contents.as_str());
     let tokens = l.run();
     println!("Tokens:\n{}", tokens);
     let mut p = parser::Parser::new(&tokens);
 
     let result = p.parse();
+    println!("{} took {:?}", file_name, start.elapsed());
 
     return result;
-
-    // match output {
-    //     Ok(o) => println!("Success:\n{}", o),
-    //     Err(e) => println!("Error: {:?}", e)
-    // }
-
-    // Ok(())
 }
